10e01ac0c025b7d82ed2f1fc0708800450ee8506
Benyamin Chehrazi <benyamin.chehrazi@gmail.com>
2020-04-05 15:44:18 +0430

    initial commit

---
+++ js/main.js
@@ -0,0 +1,114 @@
+const choices = document.querySelectorAll('.choice');
+const score = document.getElementById('score');
+const result = document.getElementById('result');
+const restart = document.getElementById('restart');
+const modal = document.querySelector('.modal');
+const scoreboard = {
+    player: 0,
+    computer: 0
+}
+
+//play game 
+function play(e) {
+    restart.style.display = 'inline-block';
+    const playerChoice = e.target.id;
+    const computerChoice = getComputerChoice();
+    const winner = getWinner(playerChoice, computerChoice);
+    showWinner(winner, computerChoice);
+
+}
+//get computer choice
+function getComputerChoice(){
+    const rand = Math.random();
+    if (rand < 0.34) {
+        return 'rock';
+    }else if (rand <= 0.67) {
+        return 'paper';
+    }else {
+        return 'scissors';
+    }
+}
+
+
+//get winner 
+
+function getWinner(p, c){
+    if(p === c){
+        return 'draw';
+    } else if(p === 'rock') {
+        if (c === 'paper') {
+            return 'computer';
+        }else {
+            return 'player';
+        }
+    }else if (p === 'paper'){
+        if (c === 'scissors'){
+            return ' computer';
+        }else {
+            return 'player';
+        }
+    }else if (p === 'scissors'){
+        if(c === 'rock'){
+            return 'computer';
+        }else {
+            return 'player';
+        }
+    }
+
+}
+
+//show winner 
+
+function showWinner(winner, computerChoice){
+    if(winner === 'player'){
+        //inc player score 
+        scoreboard.player++;
+        //show modal result 
+        result.innerHTML = `<h1 class="text-win">You Win</h1><i class="fas fa-hand-${computerChoice} fa-10x"></i>
+        <p> Computer Chose </p> <strong>${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)}</strong>
+        `;
+    }else if (winner === 'computer'){
+                //inc player score 
+                scoreboard.computer++;
+                //show modal result 
+                result.innerHTML = `<h1>You Lose</h1><i class="fas fa-hand-${computerChoice} fa-10x"></i>
+                <p> Computer Chose </p> <strong>${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)}<strong>
+                `;
+    }else {
+        result.innerHTML = `<h1> Draw </h1><i class="fas fa-hand-${computerChoice} fa-10x"></i>
+                <p> Computer Chose </p> <strong>${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)} </strong>
+                `;
+    }
+    
+    //show score 
+score.innerHTML = `<p>Player: ${scoreboard.player}</p>
+<p>Computer: ${scoreboard.computer}</p>
+`;
+
+modal.style.display = 'block';
+}
+
+//restart game 
+
+// Restart game
+function restartGame() {
+  scoreboard.player = 0;
+  scoreboard.computer = 0;
+  score.innerHTML = `<p>Player: 0</p>
+    <p>Computer: 0</p>
+  `;
+}
+
+//clear modal 
+function clearModal(e){
+    if(e.target == modal) {
+        modal.style.display = 'none';
+    }
+}
+
+
+//event listener 
+
+choices.forEach(choice => choice.addEventListener('click', play));
+window.addEventListener('click', clearModal); // clicking every where is clean the modal
+restart.addEventListener('click', restartGame);
